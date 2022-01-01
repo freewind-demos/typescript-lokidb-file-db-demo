517cdfb7b28305ca1fcbeeeef433e98f8ffc89ed
Freewind <nowindlee@gmail.com>
2022-01-01 22:01:05 +0800

    typescript-lokidb-file-db-demo

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-TypeScript Hello World Demo
+TypeScript LokiDb File Db Demo
 ===========================
+
+LokiDb是一个文件型数据库。
+
+默认保存在memory，需要使用`@lokidb/fs-storage`并调用`db.saveDatabase()`保存到文件中。
+
+注意：
+1. 没有提供一个确认数据库是否存在的函数，loadDatabase时会报错
+2. 需要调用`saveDatabase`保存一个空的数据库，里面实际上是一些元数据
 
 ```
 npm install
 npm run demo
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,39 @@
-console.log('Hello');
+import LokiDb from '@lokidb/loki';
+import {FSStorage} from '@lokidb/fs-storage';
+
+type Post = {
+  title: string;
+}
+
+async function hello() {
+  FSStorage.register();
+  const db = new LokiDb("db/db.json");
+  await db.initializePersistence({
+    adapter: new FSStorage()
+  });
+
+  try {
+    await db.loadDatabase();
+  } catch (error) {
+    console.error("### loadDatabase error", error);
+  }
+
+  const posts = db.addCollection<Post>('posts');
+
+  posts.insert({title: 'hello1'});
+  posts.insert({title: 'hello2'});
+  posts.insert({title: 'hello3'});
+  posts.insert({title: 'hello4'});
+
+  const hello3 = posts.findOne({title: 'hello3'});
+  hello3.title = 'hello33'
+  posts.update(hello3);
+
+  console.log("### posts", JSON.stringify(posts.find(), null, 4))
+  await db.saveDatabase();
+
+  await db.close();
+
+}
+
+hello()
